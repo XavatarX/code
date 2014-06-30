929bd6d0b5aa72528d6ad0d188d1b5a4ffbc9913
avatar <kanishk.85@gmail.com>
2014-06-30 16:23:29 +0530

    added sending mail code... also fixed couple of bugs

--- workspace/scanFS/src/com/example/scanfs/MainActivity.java
+++ workspace/scanFS/src/com/example/scanfs/MainActivity.java
@@ -6,8 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.*;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,6 +36,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button2);
+    	btn.setEnabled(false);
     }
 
     @Override
@@ -46,7 +51,7 @@ public class MainActivity extends Activity {
 
     	public Queue<File> qe;
     	private static final String TAG = "Scanning";
-    	FileOutputStream fOut;
+    	FileOutputStream fos;
     	OutputStreamWriter osw;
     	String currentDirectory;
     	public boolean isSymlink(File file) {
@@ -70,7 +75,9 @@ public class MainActivity extends Activity {
         {
     		int updateCounter = MAX_UPDATE_COUNTER;
         	while (!qe.isEmpty()) {
+        		Log.e(TAG,"Removing");
         		File f = qe.remove();
+        		Log.e(TAG,"Removed");
         		if (f.isDirectory()) {
         			currentDirectory = f.getAbsolutePath();
         			File[] list = f.listFiles();
@@ -78,8 +85,11 @@ public class MainActivity extends Activity {
         				continue;
         			}
         			for (File file : list) {
+        				Log.e(TAG,"Running---");
         				if (file.isFile()) {
+        					Log.e(TAG,"Getting details " + file.getAbsolutePath());
         	    			String s = file.getAbsolutePath() + "," + file.lastModified() + "," + Long.toString(getAccessTime(file.getAbsolutePath())) +"," + file.length();
+        	    			Log.e(TAG,"Got");
         	    			try {
         						osw.append(s);
         						Log.e(TAG, s);
@@ -89,35 +99,47 @@ public class MainActivity extends Activity {
         						break;
         					}
         				} else if (file.isDirectory() && !file.getAbsolutePath().contentEquals("/proc") && !file.getAbsolutePath().contentEquals("/sys") && !file.getAbsolutePath().contentEquals("/d") && !file.getAbsolutePath().contentEquals("/dev") && !isSymlink(file)){
+        					Log.e(TAG,"Adding dir" + file.getAbsolutePath());
         					qe.add(file);
+        					Log.e(TAG,"Added");
         				}
         				updateCounter--;
         				if (updateCounter == 0) {
+        					Log.e(TAG,"Publishing");
         					publishProgress(null);
+        					Log.e(TAG,"Published");
         					updateCounter = MAX_UPDATE_COUNTER;
         				}
         			}
+        			Log.e(TAG,"Files in this directory finished");
         		}
         		try {
+        			Log.e(TAG,"Flushing");
     				osw.flush();
+    				Log.e(TAG,"Flushed");
     			} catch (IOException e) {
     				// TODO Auto-generated catch block
     				Log.e(TAG, "File Flushing failed");
     				break;
     			}
         	}
+        	Log.e(TAG,"Done with all the files");
         	try {
+//        		currentDirectory = "Please send file /scanned_data to kanishk.85@gmail.com";
+//        		publishProgress(null);
     			osw.close();
     		} catch (IOException e) {
     			// TODO Auto-generated catch block
     			Log.e(TAG, "File Close failed");
     		}
         }
-    	@Override
+    	@SuppressWarnings("deprecation")
+		@Override
         protected String doInBackground(String... params) {
         	File f = new File("/");
         	try {
-                osw = new OutputStreamWriter(openFileOutput("scanned_data", Context.MODE_PRIVATE));
+        		fos=openFileOutput("scanned_data", Context.MODE_WORLD_READABLE);
+                osw = new OutputStreamWriter(fos);
 
             }
             catch (IOException e) {
@@ -126,12 +148,26 @@ public class MainActivity extends Activity {
         	qe = new LinkedList<File>();
         	qe.add(f);
         	Iterate ();
+        	try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				Log.e(TAG, "Error in clisoing fos");
+			}
     		return "";
     	}
     	@Override
     	protected void onProgressUpdate(Void... values) {
     		TextView txt = (TextView) findViewById(R.id.textView1);
     		txt.setText(currentDirectory);
+    	}
+
+    	@Override
+    	protected void onPostExecute(String result) {
+    		TextView txt = (TextView) findViewById(R.id.textView1);
+    		txt.setText("Please send file /scanned_data to kanishk.85@gmail.com");
+    		Button btn = (Button) findViewById(R.id.button2);
+        	btn.setEnabled(true);
     	}
     }
     
@@ -195,6 +231,36 @@ public class MainActivity extends Activity {
     	Button btn = (Button) view.findViewById(R.id.button1);
     	btn.setEnabled(false);
     	
+    }
+    
+    @SuppressLint("NewApi")
+	public  void email(Context context, String emailTo, String emailCC,
+    	    String subject, String emailText,String file)
+    	{
+    	    //need to "send multiple" to get more than one attachment
+    	    final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
+    	    emailIntent.setType("text/plain");
+    	    emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, 
+    	        new String[]{emailTo});
+    	    emailIntent.putExtra(android.content.Intent.EXTRA_CC, 
+    	        new String[]{emailCC});
+    	    emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject); 
+    	    emailIntent.putExtra(Intent.EXTRA_TEXT, emailText);
+    	    //has to be an ArrayList
+    	    ArrayList<Uri> uris = new ArrayList<Uri>();
+    	    //convert from paths to Android friendly Parcelable Uri's
+
+    	    File fileIn = new File(file);
+    	    fileIn.setReadable(true, false);
+    	    Uri u = Uri.fromFile(fileIn);
+    	    uris.add(u);
+    	    emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+    	    startActivity(emailIntent);
+    	}
+    public void sendMail(View view) {
+    	email(getApplicationContext(), "kanishk.85@gmail.com", "kanishk.85@gmail.com", "Android disk usage analysis",
+    			"Disk usage analysis is attached.", "scanned_data");
+    			
     }
     
 }
